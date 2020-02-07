cebe9d5dfa270670a20107c7c293743cf9c3fe1a
kakayang <calorieyang@163.com>
2020-02-07 15:17:40 +0800

    [+]商品管理

--- src/main/java/com/ecpbm/pojo/ProductInfo.java
+++ src/main/java/com/ecpbm/pojo/ProductInfo.java
@@ -1,16 +1,19 @@
 package com.ecpbm.pojo;
 
 public class ProductInfo {
-    private int id;
-    private String code;
-    private String name;
-    private Type type;
-    private String brand;
-    private String pic;
-    private int num;
-    private double price;
-    private String intro;
-    private int status;
+    // 商品基本信息（部分）
+    private int id; // 商品编号
+    private String code; // 商品编码
+    private String name; // 商品名称
+    // 关联属性
+    private Type type;    // 商品类型
+    private String brand; // 商品品牌
+    private String pic; // 商品小图
+    private int num; // 商品数量
+    private double price; // 商品价格
+    private String intro; // 商品介绍
+    private int status; // 商品状态
+
     private double priceFrom;
     private double priceTo;
 
@@ -38,12 +41,12 @@ public class ProductInfo {
         this.name = name;
     }
 
-    public Type getType() {
-        return type;
+    public String getIntro() {
+        return intro;
     }
 
-    public void setType(Type type) {
-        this.type = type;
+    public void setIntro(String intro) {
+        this.intro = intro;
     }
 
     public String getBrand() {
@@ -52,6 +55,22 @@ public class ProductInfo {
 
     public void setBrand(String brand) {
         this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public String getPic() {
@@ -70,30 +89,6 @@ public class ProductInfo {
         this.num = num;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getIntro() {
-        return intro;
-    }
-
-    public void setIntro(String intro) {
-        this.intro = intro;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
     public double getPriceFrom() {
         return priceFrom;
     }
@@ -109,4 +104,13 @@ public class ProductInfo {
     public void setPriceTo(double priceTo) {
         this.priceTo = priceTo;
     }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
 }
